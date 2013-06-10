395ab5ac1ad9bfe1e9f842a57030ab4ff6126b68
Safayet Ahmed <gtg778w@ZeenatulDunyia.(none)>
2013-06-10 16:46:55 -0400

    Added code to the cpufreq_test module to demonstrate the frequency transition notifier mechanism. Seems to work somewhat. However, the current frequency, even after the POSTCHANGE event remains the same. The delay until the actual transition to the new frequency is not known.

--- cpufreq_test/module_main.c
+++ cpufreq_test/module_main.c
@@ -31,18 +31,85 @@ module_param
     for_each_online_cpu
 */
 
+#include <linux/notifier.h>
+/*
+    struct notifier_block
+*/
+
 MODULE_AUTHOR("Safayet N Ahmed");
 MODULE_LICENSE("Dual BSD/GPL");
 
+static int cpufreq_test_trans_notifier( struct notifier_block *nb,
+                                        unsigned long val, void *data)
+{
+    /*Data contains a pointer to a cpufreq_freqs structure.
+    The cpufreqs structure describes the previous frequency,
+    next frequency, the cpu where the transition takes place,
+    and flags related to the driver*/
+    struct cpufreq_freqs *freq = data;
+
+    unsigned int cur_freq = cpufreq_quick_get(freq->cpu);
+    
+    printk(KERN_INFO "cpufre_test: trans notifier called for cpu %i", 
+                     freq->cpu);
+    
+    /*val contains one of a number of values:
+        CPUFREQ_PRECHANGE
+        CPUFREQ_POSTCHANGE
+        CPUFREQ_RESUMECHANGE
+        CPUFREQ_SUSPENDCHANGE
+    */
+    switch(val)
+    {
+        case CPUFREQ_PRECHANGE:
+            printk(KERN_INFO "transition event: CPUFREQ_PRECHANGE");
+            break;
+            
+        case CPUFREQ_POSTCHANGE:
+            printk(KERN_INFO "transition event: CPUFREQ_POSTCHANGE");
+            break;
+        
+        case CPUFREQ_RESUMECHANGE:
+            printk(KERN_INFO "transition event: CPUFREQ_RESUMECHANGE");
+            break;
+            
+        case CPUFREQ_SUSPENDCHANGE:
+            printk(KERN_INFO "transition event: CPUFREQ_SUSPENDCHANGE");
+            break;
+            
+        default:
+            break;
+    }
+    
+    printk(KERN_INFO "\t%ukHz -> %ukHz",
+                     freq->old,
+                     freq->new);
+    printk(KERN_INFO "\tcurrent: %ukHz", cur_freq);
+    
+    return 0;
+}
+
+static struct notifier_block cpufreq_test_trans_notifier_block = 
+{
+    .notifier_call = cpufreq_test_trans_notifier
+};
+int trans_notifier_registered = 0;
+
+
 void cleanup_cpufreq_test(void)
 {
+    if(1 == trans_notifier_registered)
+    {
+        cpufreq_unregister_notifier(&cpufreq_test_trans_notifier_block,
+                                    CPUFREQ_TRANSITION_NOTIFIER);   
+    }
 }
 
 int __init init_cpufreq_test(void)
 {
-    int returnable = 0;
     int cpu;
     int f;
+    int ret = 0;
 
     unsigned int cur_freq;
 
@@ -77,8 +144,20 @@ int __init init_cpufreq_test(void)
         printk(KERN_INFO "\tcurrent CPU frequency:");
         printk(KERN_INFO "\t\t%u", cur_freq);
     }
+
+    ret = cpufreq_register_notifier(&cpufreq_test_trans_notifier_block,
+                                    CPUFREQ_TRANSITION_NOTIFIER);
+    if(0 != ret)
+    {
+        trans_notifier_registered = 0;
+        printk(KERN_INFO "\tcpufreq_register_notifier failed!\n");
+    }
+    else
+    {
+        trans_notifier_registered = 1;
+    }
     
-    return returnable;
+    return ret;
 }
 
 module_init(init_cpufreq_test);
