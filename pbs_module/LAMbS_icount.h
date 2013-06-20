258fab59ec1c6f6d9086eac955cd430a6c95ed92
Michael Giardino <mikelj@gmail.com>
2013-06-20 13:05:52 -0400

    Added kernel instruction counting code and fixed a bug whereby counter creation wasn't actually being tested

--- pbs_module/LAMbS_icount.h
+++ pbs_module/LAMbS_icount.h
@@ -7,15 +7,19 @@
 typedef struct LAMbS_icount_s
 {
     u64 icount;
+    u64 icount_kernel;
     /* do we want these? running is time running, enabled is time enabled*/ 
     u64 running;
     u64 enabled;
 } LAMbS_icount_t;
 
 extern struct perf_event_attr icount_attr;
+extern struct perf_event_attr icount_attr_kernel;
+extern struct perf_event* icount_event_kernel;
 extern struct perf_event* icount_event;
+extern bool debug;
 
-int LAMbS_icount_init(void);
+int LAMbS_icount_init(bool debug);
 void LAMbS_icount_uninit(void);
 
 void LAMbS_icount_get(LAMbS_icount_t* mostat);
