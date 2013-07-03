53224a01d909ba99092faabafccea9d8c84b4b32
Michael Giardino <mikelj@gmail.com>
2013-07-03 15:59:04 -0400

    added scripts

--- system_scripts/install_governor.sh
+++ system_scripts/install_governor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 insmod gov_test/gov_test.ko
 echo lambs > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-insmod perf_event_test/perf_event_test.ko
+insmod gov_test/freq_change_test.ko
 
--- system_scripts/remove_governor.sh
+++ system_scripts/remove_governor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo userspace > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-rmmod perf_event_test/perf_event_test.ko
+rmmod gov_test/freq_change_test.ko
 rmmod gov_test/gov_test.ko
 
