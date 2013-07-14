150a98f030e2ca40bd7be374a9ba37f6a3fa552f
Safayet Ahmed <gtg778w@ZeenatulDunyia.(none)>
2013-07-14 16:17:34 -0400

    libspbsSRT/pbsuser.c had to be corrected to print logging information correctly.
    Created postprocessing scripts to help analyze results of experiments for measuring and normalizing execution time and VIC and observing variability accross CPU frequencies without being affected by workload-related variability in execution time.

--- postproc_scripts/plot_ffmpeg_cpufreq_VIC_expt_results.m
+++ postproc_scripts/plot_ffmpeg_cpufreq_VIC_expt_results.m
@@ -1,17 +1,37 @@
 
     logdir = '~/Desktop/log/ffmpeg/dec.bigbuckbunnyfull.mov';
 
-    workloads = { 'freqconsthigh', 'freqconstlow', 'freqcycle' };
+    workloads = { 'freqconsthigh', 'freqconstmed', 'freqconstlow', 'freqcycle' };
                 
     workload_count = length(workloads);
     
-    Caxis_vals = [0, 6e+11, 0, 7e+07];
-    VICaxis_vals= [0, 6e+11, 0, 1.35e+08];
+    workload_log_dir =  sprintf('%s/%s', logdir, 'freqconstmed');
+    [SRT_records] =  parse_SRT_full_wVIC_dir(workload_log_dir);
+    [summ_SRT_record] = summarizeResults_SRT_run_full_wVIC( SRT_records, ...
+                                                            20, 80, 3);
+    std_C = summ_SRT_record.avg_C1;
+    std_VIC=summ_SRT_record.avg_VIC1;
+    
+    close all;
     
     for i = 1: workload_count
-        workload_log_dir = sprintf('%s/%s', logdir, workloads{i});
+        workload_log_dir =  sprintf('%s/%s', logdir, workloads{i});
         [SRT_records] = parse_SRT_full_wVIC_dir(workload_log_dir);
+        
+        Caxis_vals = [0, 2.7e+11, 0, 7e+07];
+        VICaxis_vals= [0, 2.7e+11, 0, 1.35e+08];
         plot_C_vs_VIC(  SRT_records(1), 2, workloads{i}, logdir, ...
                         Caxis_vals, VICaxis_vals);
+                        
+        [summ_SRT_record] =summarizeResults_SRT_run_full_wVIC(  SRT_records, ...
+                                                                20, 80, 3);
+                                                                
+        norm_plot_filename =sprintf('norm_%s', workloads{i});
+        
+        Caxis_vals = [0, 2.7e+11, 0, 2];
+        VICaxis_vals= [0, 2.7e+11, 0, 2];
+        plot_norm_C_vs_VIC( summ_SRT_record, std_C, std_VIC, ...
+                            norm_plot_filename, logdir, ...
+                            Caxis_vals, VICaxis_vals);
     end
     
---
+++ postproc_scripts/plot_norm_C_vs_VIC.m
@@ -0,0 +1,41 @@
+function [] = plot_norm_C_vs_VIC(   summ_SRT_record, std_C, std_VIC, ...
+                                    plot_name, filedir, ...
+                                    Caxis_vals, VICaxis_vals)
+
+
+    norm_p1_C = summ_SRT_record.p1_C1 ./ std_C;
+    norm_avg_C= summ_SRT_record.avg_C1 ./ std_C;
+    norm_p2_C = summ_SRT_record.p2_C1 ./ std_C;
+    
+    norm_p1_VIC =   summ_SRT_record.p1_VIC1 ./ std_VIC;
+    norm_avg_VIC =  summ_SRT_record.avg_VIC1 ./ std_VIC;
+    norm_p2_VIC =   summ_SRT_record.p2_VIC1 ./ std_VIC;
+
+    t = summ_SRT_record.rel_release_time;
+    
+    plot_title = sprintf('Plot of normalized C and VIC against t for %s', plot_name);
+    
+    fig_h = figure();
+        
+    subplot(2, 1, 1);
+    hold on
+    plot(t, norm_p1_C);
+    plot(t, norm_avg_C);
+    plot(t, norm_p2_C);
+    xlabel('Time (ns)');
+    ylabel('Normalized Execution Time');
+    axis(Caxis_vals);
+    
+    title_h = title(plot_title);
+    
+    subplot(2, 1, 2);
+    plot(t, norm_p1_VIC);
+    plot(t, norm_avg_VIC);
+    plot(t, norm_p2_VIC);
+    xlabel('Time (ns)');
+    ylabel('Normalized VIC');
+    axis(VICaxis_vals);
+    
+    figurename = sprintf('%s/%s.jpg', filedir, plot_name);
+    print (fig_h, figurename, '-djpg');
+end
