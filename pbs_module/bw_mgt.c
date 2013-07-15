536fee2a2df1bea46eb7546d2757f8fdd5e2011c
Michael Giardino <mikelj@gmail.com>
2013-07-15 15:44:39 -0400

    added commented LAMbS_cpufreq_schedule

--- pbs_module/bw_mgt.c
+++ pbs_module/bw_mgt.c
@@ -196,7 +196,7 @@ ssize_t bw_mgt_write(   struct file *filep,
                 //function "sp_timer_func" this is done to ensure that expires_next 
                 //and expires_prev are initialized by it
                 allocator_state = ALLOCATOR_LOOP;
-
+		/* LAMbS_cpufreq_schedule(LAMbS_mo_sched);*/:wq
                 allocator_sleep();
                 
                 //This is the start of the first scheduling period
