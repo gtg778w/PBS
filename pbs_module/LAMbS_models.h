3aa81866e6c4bd9cdd1d804ed2cd382a84bccf06
Safayet Ahmed <gtg778w@ZeenatulDunyia.(none)>
2013-07-21 18:16:22 -0400

    Fixed the fixed-point multiplication function to handle signed numbers and fix some minor bugs.

--- pbs_module/LAMbS_models.h
+++ pbs_module/LAMbS_models.h
@@ -25,18 +25,24 @@ void    LAMbS_models_free_pages(void);
 void    LAMbS_models_init(void);
 void    LAMbS_models_process_omschedule(void);
 
-static inline u64 LAMBS_models_multiply_shift(u64 opA, u64 opB, unsigned int shift)
+static inline s64 LAMBS_models_multiply_shift(s64 opA, s64 opB, unsigned int shift)
 {
-    u64 hiA, loA, hiB, loB;
-    u64 hihiC, hiloC, lohiC, loloC;
-    u64 C;
+    s64 hiA, loA, hiB, loB, signA, signB;
+    s64 hihiC, hiloC, lohiC, loloC;
+    s64 signC, C;
     
+    /*Extract and check the sign bit of the operand and negate the operand if necessary*/
+    signA   = (opA >> 63);
+    opA = (signA != 0)? -opA : opA;
     /*Extract the hi and lo parts of the operands*/
-    /*All operands are unsigned. There should be no sign extension*/
-    hiA = (opA >> 32);
+    hiA = (opA >> 32) & 0xffffffff;
     loA = (opA) & 0xffffffff;
 
-    hiB = (opB >> 32);
+    /*Extract and check the sign bit of the operand and negate the operand if necessary*/
+    signB   = (opB >> 63);
+    opB = (signB != 0)? -opB : opB;
+    /*Extract the hi and lo parts of the operands*/
+    hiB = (opB >> 32) & 0xffffffff;
     loB = (opB) & 0xffffffff;
     
     /*initial multiply operation: 128bit C = 64bit A * 64bit B*/
@@ -48,7 +54,9 @@ static inline u64 LAMBS_models_multiply_shift(u64 opA, u64 opB, unsigned int shi
     /*Shift the parts as appropriate according to the shift argument. It is assumed the
     shift argument is less than 64*/
     /*If this function is inlined and the shift operand is assigned from a constant, then 
-    it should be possible to optimize out the branch statements.
+    it should be possible to optimize out the branch statements. Even if the branch is 
+    not optimized out, the consistent branch direction should allow for good branch
+    predictor performance.
     */
     hihiC = hihiC << (64 - shift);
     if(shift > 32)
@@ -64,6 +72,12 @@ static inline u64 LAMBS_models_multiply_shift(u64 opA, u64 opB, unsigned int shi
     loloC = loloC >> shift;
     
     C = hihiC + hiloC + lohiC + loloC;
+    
+    /*The sign of the output should only be negative if signA and signB or unequal*/
+    signC   = signA ^ signB;
+    
+    /*Check signC and negate if necessary*/
+    C = (signC != 0)? -C : C;
     
     return C;
 }
