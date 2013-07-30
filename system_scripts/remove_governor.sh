c808dc00f22b586c74eb3748a3bb52c3e6ec09a8
Michael Giardino <mikelj@gmail.com>
2013-07-29 22:37:29 -0400

    updated governor system scripts

--- system_scripts/remove_governor.sh
+++ system_scripts/remove_governor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo userspace > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-rmmod gov_test/freq_change_test.ko
-rmmod gov_test/gov_test.ko
+rmmod freq_change_test
+rmmod LAMbS_governor 
 
