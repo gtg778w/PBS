7507f06fff86bb90654cfa5ea105eb0d5a9a3f09
Safayet Ahmed <gtg778w@ZeenatulDunyia.(none)>
2013-07-30 15:29:58 -0400

    The plotting script for LAMbS_VICtimer test was modified.

--- LAMbS_VICtimer_test/plot_VICtimer_test_results.m
+++ LAMbS_VICtimer_test/plot_VICtimer_test_results.m
@@ -10,7 +10,7 @@ function [] = plot_VICtimer_test_results(results_name)
     [x_boundaries, fbar] = discretized_pdf(VIC_error(1:end), 50);
     figure(1);
     bar_vw(x_boundaries, fbar);
-    %axis([-5000, 5000, -0.0005, 0.002]);
+    axis([-5000, 5000, -0.0005, 0.002]);
     
     target_ns = raw_data(:,5);
     callback_ns = raw_data(:,6);
@@ -18,7 +18,7 @@ function [] = plot_VICtimer_test_results(results_name)
     [x_boundaries, fbar] = discretized_pdf(ns_error(1:end), 50);
     figure(2);
     bar_vw(x_boundaries, fbar);
-    %axis([-5000, 5000, -0.0005, 0.002]);
+    axis([-5000, 5000, -0.0005, 0.002]);
     
     LAMbS_ns_threshold = 3000;
     LAMbS_FP_gain = 2 ^ 48;
@@ -29,6 +29,6 @@ function [] = plot_VICtimer_test_results(results_name)
     [x_boundaries, fbar] = discretized_pdf(VIC_model_error(1:end), 50);
     figure(3);
     bar_vw(x_boundaries, fbar);
-    %axis([-5000, 5000, -0.0005, 0.002]);
+    axis([-5000, 5000, -0.0005, 0.002]);
     
 end
