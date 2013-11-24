d75c838295fb55ce9d27deb96f2a0754abbaebcd
Safayet Ahmed <gtg778w@ZeenatulDunyia.(none)>
2013-11-24 10:41:23 -0500

    Minor changes to the post-processing scripts for the budget test.

--- TSP_budget_test/expt_proc_scripts/main.m
+++ TSP_budget_test/expt_proc_scripts/main.m
@@ -3,19 +3,19 @@
     root_dir = '../budget_data/ffmpeg/';
     workload_list = {   'dec.arthur.spx', ...
                         'dec.mozart.mp3', ...
-                        ...%'dec.sintel.mp4', ...
+                        'dec.sintel.mp4', ...
                         'enc.arthur.wav.amr', ...
                         'enc.beethoven.wav.mp3', ...
                         'enc.highway.y4m.mp4'};
     
-    workload_count = 5;
+    workload_count = 6;
     
+    colors = {[1.0, 0, 0], [0, 0, 1.0], [0, 0, 0]};
+        
     for w_i = 1:workload_count
     
         workload_dir = sprintf('%s/%s', root_dir, workload_list{w_i});
         series_set = get_series_set(workload_dir);
-        
-        colors = {[1.0, 0, 0], [0, 1.0, 0], [0, 0, 0]};
         
         subplot(2, 3, w_i);
         [handles] = scatter_series_set( series_set.series_names, ...
--- TSP_budget_test/expt_proc_scripts/scatter_series_set.m
+++ TSP_budget_test/expt_proc_scripts/scatter_series_set.m
@@ -19,6 +19,10 @@ function [handles] = scatter_series_set(series_names, x_vals, y_vals, colors)
     hold on;
     for index = 1:N
         handles{index} = scatter(x_vals{index}, y_vals{index}, [], colors{index});
+        printf('series name: %s, color: (%f, %f, %f)\n',    series_names{index}, ...
+                                                            colors{index}(1), ...
+                                                            colors{index}(2), ...
+                                                            colors{index}(3));
     end
     hold off;
 end
