3fd3e509ac36dcb5cceb77a7a5dff31e35f6b18b
Safayet Ahmed <gtg778w@ZeenatulDunyia.(none)>
2013-11-25 11:38:46 -0500

    Modified post-processing scripts. Modified to output a file called "alpha.txt" that lists workloads, predictors, alpha values, and budget allocations. Useful for adjusting alpha values.

--- TSP_budget_test/expt_proc_scripts/get_series_set.m
+++ TSP_budget_test/expt_proc_scripts/get_series_set.m
@@ -7,6 +7,8 @@ function [series_set] = get_series_set(workload_dir)
     series_names = cell(dir_filecount, 1);
     alphas = cell(dir_filecount, 1);
     miss_rates = cell(dir_filecount, 1);
+    mean_miss_rates = cell(dir_filecount, 1);
+    mean_allocated_budget = cell(dir_filecount, 1);
     RMS_VFT_error = cell(dir_filecount, 1);
     allocated_budget = cell(dir_filecount, 1);
     budget_utilization = cell(dir_filecount, 1);
@@ -40,6 +42,9 @@ function [series_set] = get_series_set(workload_dir)
         dir_filecount2= length(dir_filelist2);
         
         alphas{s} = zeros(dir_filecount2, 1);
+        mean_miss_rates{s} = zeros(dir_filecount2, 1);
+        mean_allocated_budget{s} = zeros(dir_filecount2, 1);
+        
         miss_rates{s} = [];
         RMS_VFT_error{s} = [];
         allocated_budget{s} = [];
@@ -72,23 +77,35 @@ function [series_set] = get_series_set(workload_dir)
             
             SRT_records = process_SRT_rundir(file_path2);
             
-            miss_rates{s}       = [miss_rates{s}, (SRT_records.total_misses/SRT_records.job_count)];
-            RMS_VFT_error{s}    = [RMS_VFT_error{s}, SRT_records.RMS_VFT_error];
+            local_miss_rates = (SRT_records.total_misses/SRT_records.job_count);
+            miss_rates{s}       = [miss_rates{s}, local_miss_rates];
+            mean_miss_rates{s}(a)  = mean(local_miss_rates);
+            
+            local_allocated_budget = SRT_records.allocated_budget/SRT_records.job_count;
             allocated_budget{s} = [allocated_budget{s}, SRT_records.allocated_budget/SRT_records.job_count];
+            mean_allocated_budget{s}(a) = mean(local_allocated_budget);
+            
+            RMS_VFT_error{s}    = [RMS_VFT_error{s}, SRT_records.RMS_VFT_error];
             budget_utilization{s}   = [budget_utilization{s}, (SRT_records.consumed_budget_ns./SRT_records.allocated_budget)];
         end
         (alphas{s}) = (alphas{s})(1:a);
+        (mean_miss_rates{s}) = (mean_miss_rates{s})(1:a);
+        (mean_allocated_budget{s}) = (mean_allocated_budget{s})(1:a);
     end
 
     series_names= series_names(1:s);
     alphas      = alphas(1:s);
     miss_rates  = miss_rates(1:s);
+    mean_miss_rates = mean_miss_rates(1:s);
+    mean_allocated_budget = mean_allocated_budget(1:s);
     RMS_VFT_error   = RMS_VFT_error(1:s);
     allocated_budget= allocated_budget(1:s);
     budget_utilization  = budget_utilization(1:s);
     series_set.series_names = series_names;
     series_set.alphas       = alphas;
     series_set.miss_rates   = miss_rates;
+    series_set.mean_miss_rates  = mean_miss_rates;
+    series_set.mean_allocated_budget = mean_allocated_budget;
     series_set.RMS_VFT_error= RMS_VFT_error;
     series_set.allocated_budget = allocated_budget;
     series_set.budget_utilization   = budget_utilization;
--- TSP_budget_test/expt_proc_scripts/main.m
+++ TSP_budget_test/expt_proc_scripts/main.m
@@ -1,12 +1,15 @@
 
     figure;
-    root_dir = '../budget_data/ffmpeg/';
+    root_dir = '../data/ffmpeg/';
     workload_list = {   'dec.arthur.spx', ...
                         'dec.mozart.mp3', ...
                         'dec.sintel.mp4', ...
                         'enc.arthur.wav.amr', ...
                         'enc.beethoven.wav.mp3', ...
                         'enc.highway.y4m.mp4'};
+    
+    outfile_name = 'alphas.txt';
+    fid = fopen(outfile_name, 'w');
     
     workload_count = 6;
     
@@ -17,6 +20,21 @@
         workload_dir = sprintf('%s/%s', root_dir, workload_list{w_i});
         series_set = get_series_set(workload_dir);
         
+        fprintf(fid, '%s: \n', disp(workload_list{w_i}));
+        for s_i = 1:length(series_set.series_names)
+            fprintf(fid, '\t%s: \n', series_set.series_names{s_i});
+            
+            [alphas, sort_i]= sort(series_set.alphas{s_i});
+            mean_miss_rates = series_set.mean_miss_rates{s_i}(sort_i);
+            budget          = series_set.mean_allocated_budget{s_i}(sort_i);
+            
+            for a_i = 1:length(series_set.alphas{s_i})
+                fprintf(fid, '\t%f,\t%f,\t%f\n',alphas(a_i), ...
+                                                mean_miss_rates(a_i), ...
+                                                budget(a_i));
+            end
+        end
+                
         subplot(2, 3, w_i);
         [handles] = scatter_series_set( series_set.series_names, ...
                                         series_set.miss_rates, ...
@@ -24,4 +42,6 @@
                                         colors);
         title(workload_list{w_i});
     end
+
+    fclose(fid);
 
