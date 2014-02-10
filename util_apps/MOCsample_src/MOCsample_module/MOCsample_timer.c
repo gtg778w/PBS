19dfb88c863c3f97fd08f141ee2760e02949719d
Safayet Ahmed <gtg778w@ZeenatulDunyia.(none)>
2014-02-10 02:23:03 -0500

    Minor modifications to the write command for testing.

--- util_apps/MOCsample_src/MOCsample_module/MOCsample_timer.c
+++ util_apps/MOCsample_src/MOCsample_module/MOCsample_timer.c
@@ -1,11 +1,13 @@
-
+#include <asm/uaccess.h>
 #include "MOCsample.h"
 #include "MOCsample_timer_command.h"
 
 ssize_t MOCsample_timer_write(MOCsample_t *MOCsample_p, const char __user *src, size_t count)
 {
-    int ret;
+    MOCsample_timer_command_t command;
+    ssize_t ret = count;
     
+    /*Check that the size is correct*/
     if( count != sizeof(MOCsample_timer_command_t))
     {
         printk(KERN_INFO "MOCsample_timer_write: count must equal the size of MOCsample_timer_command_t");
@@ -13,7 +15,28 @@ ssize_t MOCsample_timer_write(MOCsample_t *MOCsample_p, const char __user *src, 
         goto error0;
     }
     
-    return count;
+    /*Copy the command into kernel space*/
+    if(copy_from_user(&command, src, count))
+    {
+        ret = -EFAULT;
+        goto error0;
+    }
+    
+    /*Check the command*/
+    switch(command.command)
+    {
+        case MOCsample_TIMER_COMMAND_START:
+            printk(KERN_INFO "MOCsample_timer_write: start command issued.");
+            break;
+        case MOCsample_TIMER_COMMAND_STOP:
+            printk(KERN_INFO "MOCsample_timer_write: stop command issued.");
+            break;
+        default:
+            printk(KERN_INFO "MOCsample_timer_write: invalid command");
+            ret = -EINVAL;
+            goto error0;
+    }
+    
 error0:
     return ret;
 }
